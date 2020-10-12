02b99f534240319bd80c43fd3d871a50848bd4af
borealRoc <xspstudyemail.com>
2020-10-12 14:19:57 +0800

    手写简版Vuex

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p @click="$store.commit('add')">counter:{{$store.state.counter}}</p>
+    <p @click="$store.dispatch('add')">async counter:{{$store.state.counter}}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
